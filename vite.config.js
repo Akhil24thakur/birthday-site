564e310d22ebdcc79628b98ff452dff5639356ef
Akhil24thakur <Akhil24thakur@users.noreply.github.com>
2026-09-05 10:55:10 +0530

    Add GitHub Pages deployment workflow

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  base: "/birthday-site/",
   plugins: [react(), tailwindcss()],
 })
